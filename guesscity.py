8441ef1e90e02329bb9f068ed0d928d61290cae3
admin <leriia-spb>
2022-03-24 17:07:15 +0300

    alice good

--- guesscity.py
+++ guesscity.py
@@ -122,4 +122,4 @@ def get_first_name(req):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(port=port, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
